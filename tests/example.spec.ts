685ccbb865b429286e809ada39d00565cf134154
TW466WZ <manoj.madhavan@gds.ey.com>
2026-05-07 15:03:10 +0530

    Commit tests/example.spec.ts changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://testautomationpractice.blogspot.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Test Automation Practice/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://testautomationpractice.blogspot.com/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.locator('#name').fill('Playwright');
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
